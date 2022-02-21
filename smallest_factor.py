0e7095e853bd67e0d9b7015af8e7918fb11580ff
Rachel Haich <roh0002@auburn.edu>
2022-02-21 12:57:06 -0600

    Added docstrings to improve help page.

--- smallest_factor.py
+++ smallest_factor.py
@@ -1,24 +1,42 @@
 #! /usr/bin/env python3
 
-# A script for getting the smallest prime factor of an integer.
+"A module for getting the smallest prime factor of an integer."
 
 import sys
 
 def get_smallest_prime_factor(n):
+    """
+    Returns the smallest integer that is a factor of `n`.
+    
+    If `n` is a prime number `None` is returned.
+
+    Parameters
+    ----------
+    n : int
+        The integer to be factored.
+
+    Returns
+    -------
+    int or None
+        The smallest integer that is a factor of `n`
+        or None if `n` is a prime.
+    """
     for i in range(2, n):
         if (n % i) == 0:
             return i
     return None
 
-if len(sys.argv) != 2:
-    sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
-n = int(sys.argv[1])
-if n < 1:
-    sys.exit(sys.argv[0] + ": Expecting a positive integer")
 
-smallest_prime_factor = get_smallest_prime_factor(n)
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
+    n = int(sys.argv[1])
+    if n < 1:
+        sys.exit(sys.argv[0] + ": Expecting a positive integer")
 
-if smallest_prime_factor is None:
-    print(n)
-else:
-    print(smallest_prime_factor)
+    smallest_prime_factor = get_smallest_prime_factor(n)
+
+    if smallest_prime_factor is None:
+        print(n)
+    else:
+        print(smallest_prime_factor)
